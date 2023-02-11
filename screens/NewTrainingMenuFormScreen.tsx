d47fbbba386c68e0e58b2f61030f2645ae74870b
nanaru <teamsinjito.nanaru@gmail.com>
2023-02-11 23:30:37 +0900

    feat トレーニング入力画面のスクリーンのみ仮実装

---
+++ screens/NewTrainingMenuFormScreen.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+import { StyleSheet, GestureResponderEvent, Text, TouchableOpacity, View } from 'react-native';
+const NewTrainingMenuFormScreen: FC = () => {
+  return <Text>aaaa</Text>;
+};
+
+export default NewTrainingMenuFormScreen;
